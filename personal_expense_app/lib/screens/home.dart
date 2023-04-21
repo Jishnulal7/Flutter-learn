eb3500a704b7829ff9bcd78e959c04dadb95ef58
Jishnulal <jishnulal9497@gmail.com>
2023-04-21 12:18:15 +0530

    Updates

--- personal_expense_app/lib/screens/home.dart
+++ personal_expense_app/lib/screens/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:personal_expense_app/utils/transactions.dart';
 
@@ -8,14 +7,14 @@ class HomePage extends StatelessWidget {
   final List<Transaction> transaction = [
     Transaction(
       id: '1',
-      title: 'Mobile Recharge',
+      title: 'Recharge',
       amount: 499,
       date: DateTime.now(),
     ),
     Transaction(
       id: '2',
-      title: 'New Mobile',
-      amount: 30099,
+      title: 'Mobile',
+      amount: 9999,
       date: DateTime.now(),
     ),
   ];
@@ -51,14 +50,33 @@ class HomePage extends StatelessWidget {
                       Container(
                         margin: const EdgeInsets.all(20),
                         padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                        child: Text(txn.amount.toString()),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.deepPurple,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          txn.amount.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.deepPurple),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(txn.title),
-                          Text(txn.date.toString())
-                          
+                          Text(
+                            txn.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15
+                            ),
+                          ),
+                          Text(
+                            txn.date.toString(),
+                          ),
                         ],
                       )
                     ],
